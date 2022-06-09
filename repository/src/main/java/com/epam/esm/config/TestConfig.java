e020305411f5dc364d85a1c0986db82ef754e412
Artsemi Kapitula <artsemi_kapitula@epam.com>
2022-06-09 22:15:57 +0300

    Removing unused classes

--- repository/src/main/java/com/epam/esm/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm.config;
-/**
- * Spring configuration for Core module (test config)
- *
- * @author Artsemi Kapitula
- * @version 1.0
- */
-public class TestConfig {
-}
--- service/src/test/java/com/epam/esm/service/ServiceTestConfig.java
+++ service/src/test/java/com/epam/esm/service/ServiceTestConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @ComponentScan(basePackages = "com.epam.esm")
 public class ServiceTestConfig {
-
 }
 
 
